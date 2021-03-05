dde66eb3d3372d1022c34c9e45e3a8975412ab15
dhdhdhdhhejwh <80098534+dhdhdhdhhejwh@users.noreply.github.com>
2021-03-05 22:26:26 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  this.sendContact(m.chat, '0', '6287871537199', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
